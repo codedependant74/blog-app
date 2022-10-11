76c0dcec8fbe4d2a51749bf894a97466711d3d87
codedependant74 <brendale74@gmail.com>
2022-10-11 16:06:49 -0400

    "updated User Router"

--- controllers/UserRouter.js
+++ controllers/UserRouter.js
@@ -5,11 +5,59 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const user = await UserModel.find({});
-    res.send(user);
+    const users = await UserModel.find({});
+    res.send(URLSearchParams);
   } catch (error) {
     console.log(error);
     res.status(403).send("Cannot get");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.id);
+    res.send(blog);
+  } catch (error) {
+    console.log(error);
+    res.status(403).send("Cannot get");
+  }
+});
+
+//POST: CREATE a New Blog
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await UserModel.create(req.body);
+    res.send(newUser);
+  } catch (error) {
+    console.log(error);
+    res.status(403).send("Cannot create");
+  }
+});
+
+// PUT: Update by ID
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { returnDocument: "after" }
+    );
+    res.send(updatedUser);
+  } catch (error) {
+    console.log(error);
+    res.status(403).send("Cannot put");
+  }
+});
+
+//Delete
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedUser = await UserModel.findByIdAndRemove(req.params.id);
+    console.log(deletedUser);
+    res.send("User Deleted");
+  } catch (error) {
+    console.log(error);
+    res.status(403).send("Cannot delete");
   }
 });
 
