4eb753073926ce9bc71dafed89b0c1e80f315ca8
codedependant74 <brendale74@gmail.com>
2022-10-18 17:19:09 -0400

    deploy

--- views/components/Navbar.jsx
+++ views/components/Navbar.jsx
@@ -2,14 +2,15 @@ const React = require("react");
 
 class Navbar extends React.Component {
   render() {
+    // const { loggedInUser } = this.props;
     return (
       <nav style={styles.container}>
         <a href="/">Home</a>
         <a href="/blog">Blogs</a>
         <a href="/blog/new">Create New Blog</a>
         <a href="/user/signup">Signin/up</a>
-        {loggedInUser && <h6>{loggedInUser}</h6>}
-        {loggedInUser && <a href="/user/signout">Signout</a>}
+        {/* {loggedInUser && <h6>{loggedInUser}</h6>}
+        {loggedInUser && <a href="/user/signout">Signout</a>} */}
       </nav>
     );
   }
